bb59110070d5eea34e0f788a6ce8689ca5215e74
Vasyl Borsuk <borsukvasyl>
2021-05-20 10:12:58 +0300

    removed unused file

--- tests/model_training/models/test_classifier.py
+++ tests/model_training/models/test_classifier.py
@@ -1,23 +1,12 @@
 import cv2
 import torch
-import yaml
 from skimage.io import imread
 
-from bone_xray.classifier import ClassificationPredictor
+from bone_xray.classifier import Densenet121ClassificationPredictor
 
 
 def test_classifier():
-    config = yaml.load("""
-        img_size: 384
-        model:
-            name: classifier
-            backbone: densenet121
-            num_classes: 2
-            head: simple
-            pretrained: False
-    """)
-    CHECKPOINT_PATH = 'tests/files/epoch=26-step=124226.ckpt'
-    classifier = ClassificationPredictor(config, CHECKPOINT_PATH)
+    classifier = Densenet121ClassificationPredictor()
     img = imread('tests/fixtures/image1.png')
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     pred = classifier(img)
