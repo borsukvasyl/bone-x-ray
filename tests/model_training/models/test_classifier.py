361281df93956b000ffe233dca7b577415bfa367
Vasyl Borsuk <borsukvasyl>
2021-05-20 10:26:06 +0300

    added more tests

--- tests/model_training/models/test_classifier.py
+++ tests/model_training/models/test_classifier.py
@@ -1,3 +1,5 @@
+import unittest
+
 import cv2
 import torch
 from skimage.io import imread
@@ -5,9 +7,12 @@ from skimage.io import imread
 from bone_xray.classifier import Densenet121ClassificationPredictor
 
 
-def test_classifier():
-    classifier = Densenet121ClassificationPredictor()
-    img = imread('tests/fixtures/image1.png')
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    pred = classifier(img)
-    assert pred.dtype == torch.float32
+class ClassificationTestCase(unittest.TestCase):
+    def test_model(self) -> None:
+        classifier = Densenet121ClassificationPredictor()
+        img = imread('tests/fixtures/image1.png')
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        pred = classifier(img)
+
+        self.assertEqual(pred.dtype, torch.float32)
+        self.assertTrue(0 <= pred <= 1)
