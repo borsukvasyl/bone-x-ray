607689b38268f946f85aacd108d018b11578eda6
Oleksandra <56920003+oleksandra-k@users.noreply.github.com>
2021-05-21 23:49:52 +0300

    add test for data

--- tests/bone-xray/test_data.py
+++ tests/bone-xray/test_data.py
@@ -1,4 +1,16 @@
-import glob
-import os
+from bone_xray.data import parse_mura_dataset
 
-import pandas as pd
+
+def test_parse_mura_dataset():
+    dataset_labels_path = 'tests/data/dataset_labels.csv'
+    prefix = 'tests/data'
+    result = parse_mura_dataset(dataset_labels_path, prefix)
+    expected = [{'images':
+                 ['tests/data/MURA-v1.1/valid/XR_WRIST/patient11185/study1_positive/image1.png'],
+                 'study': 'MURA-v1.1/valid/XR_WRIST/patient11185/study1_positive/', 'label': 1, 'part': 'XR_WRIST'},
+                {'images':
+                 ['tests/data/MURA-v1.1/valid/XR_WRIST/patient11186/study1_positive/image1.png'],
+                 'study': 'MURA-v1.1/valid/XR_WRIST/patient11186/study1_positive/', 'label': 1, 'part': 'XR_WRIST'},
+                {'images': ['tests/data/MURA-v1.1/valid/XR_WRIST/patient11186/study2_positive/image1.png'],
+                 'study': 'MURA-v1.1/valid/XR_WRIST/patient11186/study2_positive/', 'label': 1, 'part': 'XR_WRIST'}]
+    assert result == expected
